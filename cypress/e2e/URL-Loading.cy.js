1a161425b7f17efb55ab6192958329cc0f0254e9
Duncan-Wood <duncanwoodpro@gmail.com>
2023-11-22 12:37:32 -0500

    Eric and Ben came in from downtown with the double RKO!!!!

--- cypress/e2e/URL-Loading.cy.js
+++ cypress/e2e/URL-Loading.cy.js
@@ -1,63 +1,44 @@
 /// <reference types="Cypress" />
 
-// beforeEach(() => {
-//   // cy.clearAllCookies()
-//   // cy.clearAllSessionStorage()
-//   // cy.clearAllLocalStorage()
-//   // cy.login(Cypress.env('email'), Cypress.env('password'));
-//   // cy.log("Logged in");
-// });
-
-it("Visits the home page", () => {
-  cy.visit("https://ecomapmetamap.com/");
-  cy.contains('Welcome to Meta Map')
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
 });
 
-it("Visits the about page", () => {
-  cy.visit("https://ecomapmetamap.com/about/");
-  cy.contains('Tech For All')
+beforeEach(() => {
+  cy.login(Cypress.env('email'), Cypress.env('password'));
+  cy.log("Logged in");
 });
 
-it("Visits the organizations page", () => {
-  cy.visit("https://ecomapmetamap.com/organizations");
-  cy.contains('Add Organization')
-});
-
-// describe("Load a URL and Check an Element", () => {
-//   it("Visits the home page", () => {
-//     cy.visit("https://ecomapmetamap.com/");
-//     cy.contains('Welcome to Meta Map')
-//   });
+describe("Load a URL and Check an Element", () => {
+  it("Visits the home page", () => {
+    cy.visit("https://ecomapmetamap.com/");
+    cy.contains('Welcome to Meta Map')
+  });
   
-//   it("Visits the about page", () => {
-//     cy.visit("https://ecomapmetamap.com/about/");
-//     cy.contains('Tech For All')
-//   });
+  it("Visits the about page", () => {
+    cy.visit("https://ecomapmetamap.com/about/");
+    cy.contains('Tech For All')
+  });
+  
+  it("Visits the organizations page", () => {
+    cy.visit("https://ecomapmetamap.com/organizations");
+    cy.contains('Add Organization')
+  });
 
-//   // The organizations pages are not working. They are redirecting to different pages
+  it("Visits the organizations - service providers page", () => {
+    cy.visit("https://ecomapmetamap.com/organizations/service-providers");
+    cy.contains('Add Service Provider')
+  });
 
-//   it("Visits the organizations page", () => {
-//     cy.visit("https://ecomapmetamap.com/organizations");
-//     cy.contains('Add Organization')
-//   });
+  it("Visits the organizations - resource providers page", () => {
+    cy.visit("https://ecomapmetamap.com/organizations/resource-providers");
+    cy.contains('Add Organization')
+  });
 
-  // it("Visits the organizations - service providers page", () => {
-  //   // cy.clearAllCookies()
-  //   cy.visit("https://ecomapmetamap.com/organizations/service-providers");
-  //   cy.contains('Add Service Providers')
-  // });
-
-  // it("Visits the organizations - resource providers page", () => {
-  //   // cy.clearAllCookies()
-  //   cy.visit("https://ecomapmetamap.com/organizations/resource-providers");
-  //   cy.contains('Add Organization')
-  // });
-
-  // it("Visits the organizations - businesses page", () => {
-  //   // cy.clearAllCookies()
-  //   cy.visit("https://ecomapmetamap.com/organizations/businesses");
-  //   cy.contains('Add Organization')
-  // });
+  it("Visits the organizations - businesses page", () => {
+    cy.visit("https://ecomapmetamap.com/organizations/businesses");
+    cy.contains('Add Organization')
+  });
 
 
   // The advertise page appears to be deprecated on the Metamap
@@ -67,4 +48,4 @@ it("Visits the organizations page", () => {
   //   cy.contains('404')
   // });
   
-// });
+});
